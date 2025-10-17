d7c55675626f1bb7ddcdf3680a2cd84b5f0fa90a
AndriiPyrohovskyi <cbkgfst@gmail.com>
2025-10-17 12:14:03 +0200

    Added colors and typography static files

---
+++ src/theme/colors.ts
@@ -0,0 +1,23 @@
+const colors = {
+    primary: '#C4B5FD',
+    primary15: 'rgba(196, 181, 253, 0.15)',
+    primary70: 'rgba(196, 181, 253, 0.7)',
+    dark_primary: '#A78BFA',
+    dark_primary15: 'rgba(167, 139, 250, 0.15)',
+    dark_primary70: 'rgba(167, 139, 250, 0.7)',
+    text: '#1D2839',
+    text70: 'rgba(29, 40, 57, 0.7)',
+    green: '#48D87D',
+    green15: 'rgba(72, 216, 125, 0.15)',
+    green70: 'rgba(72, 216, 125, 0.7)',
+    yellow: '#F4C714',
+    yellow15: 'rgba(244, 199, 20, 0.15)',
+    yellow70: 'rgba(244, 199, 20, 0.7)',
+    coral: '#E76565',
+    coral15: 'rgba(231, 101, 101, 0.15)',
+    coral70: 'rgba(231, 101, 101, 0.7)',
+    background: '#EFF4F9',
+    card_surface: '#FFFFFF',
+    border_divider: '#DCE2EA',
+};
+export default colors;
---
+++ src/theme/typography.ts
@@ -0,0 +1,52 @@
+const typography = {
+    h1: {
+        fontFamily: "'Poppins', sans-serif",
+        fontSize: '28px',
+        fontWeight: 700,
+        lineHeight: 1.3,
+    },
+    h2: {
+        fontFamily: "'Poppins', sans-serif",
+        fontSize: '22px',
+        fontWeight: 600,
+        lineHeight: 1.3,
+    },
+    h3: {
+        fontFamily: "'Nunito', sans-serif",
+        fontSize: '18px',
+        fontWeight: 500,
+        lineHeight: 1.3,
+    },
+    main: {
+        fontFamily: "'Nunito', sans-serif",
+        fontSize: '15px',
+        fontWeight: 400,
+        lineHeight: 1.3,
+    },
+    secondary: {
+        fontFamily: "'Inter', sans-serif",
+        fontSize: '13px',
+        fontWeight: 400,
+        lineHeight: 1.3,
+    },
+    secondary2: {
+        fontFamily: "'Inter', sans-serif",
+        fontSize: '10px',
+        fontWeight: 400,
+        lineHeight: 1.3,
+    },
+    numbers: {
+        fontFamily: "'Roboto', sans-serif",
+        fontSize: '18px',
+        fontWeight: 700,
+        lineHeight: 1.1,
+    },
+    CTA: {
+        fontFamily: "'Inter', sans-serif",
+        fontSize: '16px',
+        fontWeight: 600,
+        lineHeight: 1.2,
+    }
+};
+
+export default typography;
